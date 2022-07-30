f66ff1631d22c804035606eb75e522836c8f52d1
Karim Ayman <nighthawk1486@gmail.com>
2022-07-30 19:27:00 +0200

    fixing the api build script

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,5 @@ export AWS_BUCKET=arn:aws:s3:::full-stack-app
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=Hostingafullstackapp-env-1.eba-t2dxamrx.us-east-1.elasticbeanstalk.com
+export PORT=5000
